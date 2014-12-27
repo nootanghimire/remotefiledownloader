eb4b63363b5c06c83b897785757860fdb2433e77
Samundra Shrestha <samundra.shr@gmail.com>
2014-12-27 11:43:55 +0545

    code cleanup

--- tests/FileDownloaderTest.php
+++ tests/FileDownloaderTest.php
@@ -29,23 +29,14 @@ class FileDownloaderTest extends PHPUnit_Framework_TestCase
         array_push($this->sources, 'http://202.63.240.10/userportal/pages/images/inventlogo.png');
         array_push($this->sources, 'http://i.ytimg.com/vi/zlC_KIPw1uo/0.jpg');
 
-        //$preserveFilename = true;
-
         $this->downloader = new RemoteFileDownloader('prefix', 'suffix', $this->preserveFilename);
 
         // Change this to your target folder
         $this->testDownloadPath = 'testDownloads/';
 
-        // Set mode for newly created files
-//        $mode = 0755;
-
-        // Create target folder if it does ont exists, recursively
-//        $recursive = true;
-
         // Finally set the target where files will be downloaded
         $this->downloader->set_destination_path($this->testDownloadPath, $this->mode, $this->recursive);
 
-        //$obj = new Stdclass;
         // Add your source files, images
         $this->downloader->set_sources($this->sources);
     }
